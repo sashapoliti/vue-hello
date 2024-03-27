245f270f4b834c44b122fb9ee53cfbb50be1e723
sashapoliti <sas.politi@gmail.com>
2024-03-27 16:40:45 +0100

    add image with VueJS

--- js/script.js
+++ js/script.js
@@ -3,7 +3,11 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Happiny!'
+        message : 'Happiny!',
+        image : {
+            name : 'Pokémon Happiny',
+            media : 'img/Happiny.png'
+        }
       }
     }
   }).mount('#app')
